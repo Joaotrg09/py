037a636423d63d3167b79f29631b42619ba20b17
Joaotrg09 <joao.lima@targitbrasil.com>
2025-06-23 16:07:43 -0300

    feat: :zap: deploy3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, status, Depends
 from sqlalchemy.orm import Session
 
-from app import schemas
+from app import schemas, models
 
 from typing import List
 
@@ -20,9 +20,14 @@ def read_root():
 )
 def create_user(user: schemas.UserRequest, db: Session = Depends(get_db)):
 
-    db.add(user)
+    print("Creating user:", user)
+    print("Database session:", db)
+
+    user_create = models.User(name=user.name, email=user.email)
+
+    db.add(user_create)
     db.commit()
-    db.refresh(user)
+    db.refresh(user_create)
 
     return user
 
--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 import uuid
-
-import sqlalchemy as Column, String
+from sqlalchemy import String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db import Base
 
@@ -9,6 +9,8 @@ from app.db import Base
 class User(Base):
     __tablename__ = "users"
 
-    id: Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
-    name: Column(String, nullable=False)
-    email: Column(String, nullable=True)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
+    )
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=True)
